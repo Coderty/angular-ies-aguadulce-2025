dad058ed1c358aa5111fd5a4df8e2ccd2723c4c4
alvarompywf <alvaromenaparra@gmail.com>
2025-02-12 09:57:45 +0100

    demo componente pokemon-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokemonDetailComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-ies-aguadulce-2025';
